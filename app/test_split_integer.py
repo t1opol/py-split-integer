d2c88c56f7a1f2516a368f2365336075f504466f
Oleksandr Chuhuievets <aleksandr10chu@gmail.com>
2026-08-29 07:44:35 +0300

    Solve py-split-integer task

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -19,3 +19,13 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(3, 5) == [0, 0, 1, 1, 1]
+
+
+def test_should_return_exact_number_of_parts() -> None:
+    assert len(split_integer(17, 4)) == 4
+
+
+def test_difference_between_max_and_min_parts_should_be_at_most_one() -> None:
+    result = split_integer(17, 4)
+
+    assert max(result) - min(result) <= 1
